14d5c114dbd512a38e32687d882af4eb23741e73
Andrew Zhegalik <andrew.zhegalik@gmail.com>
2026-07-17 14:17:49 +0200

    productExceptSelf two-pointers

--- leetcode/two-pointers/Sources/two-pointers/two_pointers.swift
+++ leetcode/two-pointers/Sources/two-pointers/two_pointers.swift
@@ -28,4 +28,28 @@ class Solution {
         
         return true
     }
+    
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        var result: [Int] = []
+        
+        var l = 0
+        var counter = 1
+        
+        while l < nums.count {
+            result.append(counter)
+            counter *= nums[l]
+            l += 1
+        }
+        
+        counter = 1
+        var r = result.count - 1
+        
+        while r >= 0 {
+            result[r] = counter * result[r]
+            counter = counter * nums[r]
+            r -= 1
+        }
+        
+        return result
+    }
 }
