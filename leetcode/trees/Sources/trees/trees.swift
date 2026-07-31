f2e2b7fafa90075a6796d994a472e1b44fdca6bf
Andrew Zhegalik <andrew.zhegalik@gmail.com>
2026-07-31 18:34:29 +0200

    binary tree merge binary trees, is balanced binary tree, are the same trees

--- leetcode/trees/Sources/trees/trees.swift
+++ leetcode/trees/Sources/trees/trees.swift
@@ -2,7 +2,7 @@
 // https://docs.swift.org/swift-book
 
 class Solution {
-    func invertBinaryTree(_ root: TreeNode?) -> TreeNode? {
+    func invertBinaryTree(_ root: TreeNode?) -> TreeNode? { // Easy
         guard let root else { return nil }
         
         let temp = root.left
@@ -15,12 +15,62 @@ class Solution {
         return root
     }
     
-    func maxDepth(_ root: TreeNode?) -> Int {
+    func maxDepth(_ root: TreeNode?) -> Int { // Easy
         guard let root else { return 0 }
         
         let depth = 1 + max(maxDepth(root.left), maxDepth(root.right))
         
         return depth
+    }
+    
+    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool { // Easy
+        guard let p, let q else {
+            return p == nil && q == nil
+        }
+        
+        return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right)
+    }
+    
+    func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
+        if root1 == nil && root2 == nil {
+            return nil
+        }
+        
+        let val1: Int
+        let val2: Int
+        
+        if let root1 {
+            val1 = root1.val
+        }
+        
+        if let root2 {
+            val2 = root2.val
+        }
+        
+        let root = TreeNode(val1 + val2)
+        
+        root.left = mergeTrees(root1?.left, root2?.left)
+        root.right = mergeTrees(root1?.right, root2?.right)
+        
+        return root
+    }
+    
+    func isBalanced(_ root: TreeNode?) -> Bool {
+        return height(root) != -1
+    }
+    
+    private func height(_ node: TreeNode?) -> Int {
+        guard let node else { return 0 }
+        
+        let left = height(node.left)
+        if left == -1 { return -1 }
+        
+        let right = height(node.right)
+        if right == -1 { return -1 }
+        
+        if abs(right - left) > 1 { return -1 }
+        
+        return 1 + max(left, right)
     }
 }
 
