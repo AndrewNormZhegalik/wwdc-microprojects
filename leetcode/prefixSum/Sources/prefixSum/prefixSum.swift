dfb576dde87c968eb02fef655f55517902399a4f
Andrew Zhegalik <andrew.zhegalik@gmail.com>
2026-08-11 23:45:14 +0200

    prefix sum pivot index

--- leetcode/prefixSum/Sources/prefixSum/prefixSum.swift
+++ leetcode/prefixSum/Sources/prefixSum/prefixSum.swift
@@ -35,4 +35,41 @@ class Solution {
             prefixes[right + 1] - prefixes[left]
         }
     }
+    
+    func longestConsequtive(_ nums: [Int]) -> Int {
+        guard !nums.isEmpty else { return 0 }
+        var length: Int = 1
+        var start = 0
+        
+        for i in 1..<nums.count {
+            var currentLength = 0
+            if nums[i] > nums[i - 1] {
+                currentLength = i - start + 1
+            } else {
+                start = i
+                currentLength = 1
+            }
+            
+            length = max(currentLength, length)
+        }
+        
+        return length
+    }
+    
+    //Find Pivot Index
+    func pivotIndex(_ nums: [Int]) -> Int { // Easy
+        var prefixes: [Int] = [0]
+        
+        for num in nums {
+            prefixes.append(prefixes.last! + num)
+        }
+        
+        for i in 0..<nums.count {
+            if prefixes[i] == prefixes[nums.count] - prefixes[i + 1] {
+                return i
+            }
+        }
+        
+        return -1
+    }
 }
